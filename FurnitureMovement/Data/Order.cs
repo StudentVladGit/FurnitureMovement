8db630c87e779be3bfb5d5fb8cfa19ce4e67343f
VladislavShulzhenko <vladka_2016@mail.ru>
2024-10-18 13:16:45 +0300

    Создали классы и миграцию

---
+++ FurnitureMovement/Data/Order.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FurnitureMovement.Data
+{
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "#")]
+        public string Number {  get; set; } //Номер
+        public string Drawning { get; set; } //Схемы
+        public string Furniture { get; set; } //Оснастка
+        public DateTime AdmissionDate { get; set; } //Поступление
+        public long Quantity { get; set; } //Количество
+    }
+}
---
+++ FurnitureMovement/Migrations/20241018100810_init.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FurnitureMovement.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Number = table.Column<string>(type: "text", nullable: false),
+                    Drawning = table.Column<string>(type: "text", nullable: false),
+                    Furniture = table.Column<string>(type: "text", nullable: false),
+                    AdmissionDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Quantity = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Number);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
